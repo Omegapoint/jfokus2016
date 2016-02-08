42b6a133b30e8c74fc9821e7630c41a919f85ac8
Anna Karlsson <anna.karlsson@omegapoint.se>
2016-02-08 20:10:53 +0100

    button disable

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ requirejs(['app/app', 'agent'],
     play = document.getElementById('play');
     play.addEventListener("click", function(event) {
       document.getElementById('new_player_btn').disabled = true;
-      document.getElementById('play').disable = true;
-      document.getElementById('stop').disable = false;
+      document.getElementById('play').disabled = true;
+      document.getElementById('stop').disabled = false;
       savePlayerCode();
       saveToGameCode();
       rounds = 1;
